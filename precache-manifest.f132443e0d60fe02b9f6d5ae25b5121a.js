af46d94a861aae098f5f0a275e1b0c32755bc143
Ane-tta <favourite.ak@gmail.com>
2020-03-14 16:37:21 +0100

    Updates

---
+++ precache-manifest.f132443e0d60fe02b9f6d5ae25b5121a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea87a33b4517d386b9c0b3cd41420233",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8c237019b67f39662e00",
+    "url": "/monsters-rolodex/static/css/main.572dddc9.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c237019b67f39662e00",
+    "url": "/monsters-rolodex/static/js/main.de90f71d.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
